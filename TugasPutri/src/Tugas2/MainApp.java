78425f4ff7367412e178d0f8c006ffaa4dec3ede
putriwulandr <poetry.wd@gmail.com>
2021-07-08 22:39:49 +0700

    Tugas2

--- TugasPutri/src/Tugas2/MainApp.java
+++ TugasPutri/src/Tugas2/MainApp.java
@@ -18,110 +18,74 @@ public class MainApp
 
         do {
             System.out.println("-------- Menghitung Luas, Keliling dan Volume Bangun ----------");
-            int pilihMenuHitung, 
-                pilihLuasBangunDatar, 
-                pilihKelilingBangunDatar, 
-                pilihVolumeBangun;
+            int pilihMenuHitung;
 
             System.out.println("Pilihan Menu: \n 1. Menghitung Luas \n 2. Menghitung Keliling \n 3. Menghitung Volume Bangun\n");
             System.out.println("Masukkan Pilihan :");
             pilihMenuHitung = input.nextInt();
-            System.out.println("================");
+            System.out.println(" ");
             
             switch (pilihMenuHitung) {
                 case 1:
-                    System.out.println("Pilih Bangun Datar yang akan dihitung Luas: ");
-                    System.out.println("1. Segitiga \n2. Persegi \n3. Lingkaran\n");
-                    System.out.println("Masukkan Pilihan :");
-                    pilihLuasBangunDatar = input.nextInt();
-                    System.out.println("================");
-                    switch (pilihLuasBangunDatar) {
-                        case 1:
-                            System.out.println("Masukkan Sisi Alas : ");
-                            sgtg.setSisi_A(input.nextFloat());
-                            System.out.println("Masukkan Sisi Tinggi : ");
-                            sgtg.setSisi_T(input.nextFloat());
-                            System.out.println("Luas Segitiga : " + sgtg.luasSegitiga());
-                        break;
-                        case 2:
-                            System.out.println("Masukkan Sisi : ");
-                            prsg.setSisi(input.nextFloat());
-                            System.out.println("Luas Persegi : " + prsg.luasPersegi());
-                        break;
-                        case 3:
-                            System.out.println("Masukkan Nilai Phi : ");
-                            lgkrn.setPhi(input.nextDouble());
-                            System.out.println("Masukkan Jari-jari : ");
-                            lgkrn.setR(input.nextFloat());
-                            System.out.println("Luas Lingkaran : " + lgkrn.luasLingkaran());
-                        break;
-                        default:
-                        break;
-                    }
+                    System.out.println("//////////// MENGHITUNG LUAS /////////////");
+                    System.out.println("Masukkan Sisi Alas Segitiga : ");
+                    sgtg.setSisi_A(input.nextFloat());
+                    System.out.println("Masukkan Sisi Tinggi Segitiga : ");
+                    sgtg.setSisi_T(input.nextFloat());                    
+                    System.out.println("Masukkan Sisi Persegi : ");
+                    prsg.setSisi(input.nextFloat());                    
+                    System.out.println("Masukkan Nilai Phi Lingkaran: ");
+                    lgkrn.setPhi(input.nextDouble());
+                    System.out.println("Masukkan Jari-jari Lingkaran: ");
+                    lgkrn.setR(input.nextFloat());
+                    System.out.println(" ");
+                    System.out.println("Luas Segitiga : " + sgtg.luasSegitiga());
+                    System.out.println("Luas Persegi : " + prsg.luasPersegi());
+                    System.out.println("Luas Lingkaran : " + lgkrn.luasLingkaran());
+                    System.out.println("//////////////////////////////////////////");
+                    System.out.println(" ");
                 break;
-                case 2:
-                    System.out.println("Pilih Bangun Datar yang akan dihitung Keliling : ");
-                    System.out.println("1. Segitiga\n2. Persegi\n3. Lingkaran\n");
-                    System.out.println("Masukkan Pilihan :");
-                    pilihKelilingBangunDatar = input.nextInt();
-                    System.out.println("================");
-                    switch (pilihKelilingBangunDatar) {
-                        case 1:
-                            System.out.println("Masukkan Sisi Alas : ");
-                            sgtg.setSisi_A(input.nextFloat());
-                            System.out.println("Masukkan Sisi Tinggi : ");
-                            sgtg.setSisi_T(input.nextFloat());
-                            System.out.println("Masukkan Sisi Miring : ");
-                            sgtg.setSisi_M(input.nextFloat());
-                            System.out.println("Keliling Segitiga : " + sgtg.kelilingSegitiga());
-                        break;
-                        case 2:
-                            System.out.println("Masukkan Sisi : ");
-                            prsg.setSisi(input.nextFloat());
-                            System.out.println("Keliling Persegi : " + prsg.kelilingPersegi());
-                        break;
-                        case 3:
-                            System.out.println("Masukkan Nilai Phi : ");
-                            lgkrn.setPhi(input.nextDouble());
-                            System.out.println("Masukkan Jari-jari : ");
-                            lgkrn.setR(input.nextFloat());
-                            System.out.println("Keliling Lingkaran : " + lgkrn.kelilingLingkaran());
-                        break;
-                        default:
-                        break;
-                    }
+                case 2:  
+                    System.out.println("//////////// MENGHITUNG KELILING /////////////");                  
+                    System.out.println("Masukkan Sisi Alas Segitiga : ");
+                    sgtg.setSisi_A(input.nextFloat());
+                    System.out.println("Masukkan Sisi Tinggi Segitiga : ");
+                    sgtg.setSisi_T(input.nextFloat());
+                    System.out.println("Masukkan Sisi Miring Segitiga : ");
+                    sgtg.setSisi_M(input.nextFloat());                
+                    System.out.println("Masukkan Sisi Persegi : ");
+                    prsg.setSisi(input.nextFloat());                
+                    System.out.println("Masukkan Nilai Phi Lingkaran : ");
+                    lgkrn.setPhi(input.nextDouble());
+                    System.out.println("Masukkan Jari-jari Lingkaran : ");
+                    lgkrn.setR(input.nextFloat());
+                    System.out.println(" ");
+                    System.out.println("Keliling Segitiga : " + sgtg.kelilingSegitiga());
+                    System.out.println("Keliling Persegi : " + prsg.kelilingPersegi());
+                    System.out.println("Keliling Lingkaran : " + lgkrn.kelilingLingkaran());
+                    System.out.println("//////////////////////////////////////////");
+                    System.out.println(" ");
                 break;
                 case 3:
-                    System.out.println("Pilih Bangun yang akan dihitung Volume : ");
-                    System.out.println("1. Prisma \n2. Kubus \n3. Bola\n");
-                    System.out.println("Masukkan Pilihan :");
-                    pilihVolumeBangun = input.nextInt();
-                    System.out.println("================");
-                    switch (pilihVolumeBangun) {
-                        case 1:
-                            System.out.println("Masukkan Sisi Alas : ");
-                            prsm.setSisi_A(input.nextFloat());
-                            System.out.println("Masukkan Sisi Tinggi Alas : ");
-                            prsm.setSisi_T(input.nextFloat());
-                            System.out.println("Masukkan Tinggi Prisma : ");
-                            prsm.setTinggi(input.nextFloat());
-                            System.out.println("Volume Prisma : " + prsm.volumePrisma());
-                        break;
-                        case 2:
-                            System.out.println("Masukkan Sisi : ");
-                            kbs.setSisi(input.nextFloat());
-                            System.out.println("Volume Kubus : " + kbs.volumeKubus());
-                        break;
-                        case 3:
-                            System.out.println("Masukkan Nilai Phi : ");
-                            bolaa.setPhi(input.nextDouble());
-                            System.out.println("Masukkan Jari-jari : ");
-                            bolaa.setR(input.nextFloat());
-                            System.out.println("Volume Bola : " + bolaa.volumeBola());
-                        break;
-                        default:
-                        break;
-                    }
+                    System.out.println("/////////// MENGHITUNG VOLUME ///////////");
+                    System.out.println("Masukkan Sisi Alas Prisma: ");
+                    prsm.setSisi_A(input.nextFloat());
+                    System.out.println("Masukkan Sisi Tinggi Alas Prisma: ");
+                    prsm.setSisi_T(input.nextFloat());
+                    System.out.println("Masukkan Tinggi Prisma : ");
+                    prsm.setTinggi(input.nextFloat());
+                    System.out.println("Masukkan Sisi Kubus: ");
+                    kbs.setSisi(input.nextFloat());
+                    System.out.println("Masukkan Nilai Phi Bola: ");
+                    bolaa.setPhi(input.nextDouble());
+                    System.out.println("Masukkan Jari-jari Bola: ");
+                    bolaa.setR(input.nextFloat());
+                    System.out.println(" ");
+                    System.out.println("Volume Prisma : " + prsm.volumePrisma());
+                    System.out.println("Volume Kubus : " + kbs.volumeKubus());
+                    System.out.println("Volume Bola : " + bolaa.volumeBola());
+                    System.out.println("//////////////////////////////////////////");
+                    System.out.println(" ");
                 break;
             
                 default:
@@ -132,6 +96,8 @@ public class MainApp
         }
 
         while ((choice.equalsIgnoreCase("y")));
+        System.out.println(" ");
+        System.out.println(">>>>>>>>> Perhitungan Selesai <<<<<<<<<");
         input.close();
     }
 }
